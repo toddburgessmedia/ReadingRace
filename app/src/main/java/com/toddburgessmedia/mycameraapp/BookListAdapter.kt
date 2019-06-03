8d1e2926d21f150c1dc369e582ed2c3f5f464b65
Todd Burgess <todd@toddburgessmedia.com>
2019-06-03 13:31:00 -0400

    setup scaffolding for RecylerViewAdapter

--- app/src/main/java/com/toddburgessmedia/mycameraapp/BookListAdapter.kt
+++ app/src/main/java/com/toddburgessmedia/mycameraapp/BookListAdapter.kt
@@ -1,30 +1,47 @@
 package com.toddburgessmedia.mycameraapp
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.toddburgessmedia.mycameraapp.model.Book
+import kotlinx.android.synthetic.main.booklist_adapter.view.*
 
-class BookListAdapter(val books : List<Book>) : RecyclerView.Adapter<BookListAdapter.BookViewHolder>() {
+class BookListAdapter(val booklist : List<Book>) : RecyclerView.Adapter<BookListAdapter.BookViewHolder>() {
 
+    var books = mutableListOf<Book>()
 
+    init {
+        books.addAll(booklist)
 
-    override fun onBindViewHolder(p0: BookViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: BookViewHolder, index: Int) {
+
+        val item = books[index].items[0]
+        holder.author.setText(item.volumeInfo?.title)
+        holder.title.setText(item.volumeInfo?.authors?.get(0))
+        holder.pagecount.setText(item.volumeInfo?.pageCount?.toString())
     }
+
+
+
+    override fun getItemCount(): Int = books.size
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): BookViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        return BookViewHolder(LayoutInflater.from(p0.context)
+                    .inflate(R.layout.booklist_adapter,p0,false))
+
     }
 
     class BookViewHolder(v : View) : RecyclerView.ViewHolder(v) {
 
         private var view = v
 
+        val title  = v.booklist_title
+        val author = v.booklist_author
+        val pagecount = v.booklist_pages
 
     }
 }
